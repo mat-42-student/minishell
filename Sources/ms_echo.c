a2f4dc9d161a12c7723a69cbda144e9f751d9dbd
Walana <robin.ficht@free.fr>
2023-06-28 16:23:20 +0200

    oopsie j'avais casse le echo 👉👈

--- Sources/ms_echo.c
+++ Sources/ms_echo.c
@@ -6,16 +6,29 @@
 /*   By: rficht <robin.ficht@free.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 11:36:39 by rficht            #+#    #+#             */
-/*   Updated: 2023/06/26 09:31:33 by rficht           ###   ########.fr       */
+/*   Updated: 2023/06/28 16:18:29 by rficht           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	check_flags(char *arg, int *n_flag)
+static int	check_flags(char *arg, int *n_flag)
 {
-	if (arg[0] == '-' && arg[1] == 'n' && !arg[2])
-		*n_flag = TRUE;
+	int	n;
+
+	if (!arg)
+		return (FALSE);
+	if (arg[0] != '-')
+		return (FALSE);
+	n = 0;
+	while (arg[++n])
+	{
+		if (arg[n] == 'n')
+			*n_flag = TRUE;
+		else
+			return (FALSE);
+	}
+	return (TRUE);
 }
 
 static int	write_args(char **args, int fdout, int n_flag)
@@ -51,8 +64,8 @@ int	ms_echo(t_cmd *cmd)
 		return (1);
 	}
 	n = 1;
-	while (cmd->args[n] && cmd->args[n][0] == '-')
-		check_flags(cmd->args[n++], &n_flag);
+	while (check_flags(cmd->args[n], &n_flag))
+		n++;
 	if (write_args(cmd->args + n, cmd->fdout, n_flag))
 		return (1);
 	return (0);
